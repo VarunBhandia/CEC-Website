8a2340df94c2df07dd89079ff50d2a1686de51c0
Varun Bhandia <varunbhandia@gmail.com>
2018-01-06 05:32:02 +0530

    Navbar-done

--- index.php
+++ index.php
@@ -1,50 +1,157 @@
-<?php 
-	session_start(); 
-
-	if (!isset($_SESSION['username'])) {
-		$_SESSION['msg'] = "You must log in first";
-		header('location: login.php');
-	}
-
-	if (isset($_GET['logout'])) {
-		session_destroy();
-		unset($_SESSION['username']);
-		header("location: login.php");
-	}
-
-?>
-<!DOCTYPE html>
 <html>
-<head>
-	<title>Admin-Login-CEC</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-	<div class="content">
-
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
-
-		<!-- logged in user information -->
-		<?php  if (isset($_SESSION['username'])) : ?>
-        		
-                <p style="float:  right;"> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <head>
+        <title>CEC HOMEPAGE</title> 
+        <link href="blog.css" type="text/css" rel="stylesheet">
+        <link href="navbar.css" type="text/css" rel="stylesheet">
+    </head>
+    <style type="text/css">
+        .paratitle{
+            text-transform: uppercase;
+            color:#684cc4;
+            font-family:sans-serif;
+            font-weight:bolder;
+            
+        }
+        #about,#blog,#team{
+            position:relative;
+            margin-left:150px;
+            margin-right:150px;
+            display:block;
+            margin-top:50px;
+            font-family:sans-serif;
+            font-size: 20px;
+        }
+        #contact{
+            
+            margin-left: 150px;
+            }
+        footer{
+            background-color: #0426d3;
+            margin-left:0px;
+            margin-top:150px;
+            padding:10px;
+            line-height: 30px;
+            text-transform:capitalize;
+            color:white;
+            letter-spacing: 2px;
+            font-size: 20px;
+        }
+        .bloghead{
+            display:inline-block;
+            position:relative;
+            margin:0px;
+            float:left;
+            color:white;
+            
+        }
+        .bloghead{
+            position:relative;
+            margin-left:0px;
+            margin-top:1px;
+            margin-right:0px;
+            margin-bottom:5px;
+            display:inline;
+        }
+        .paratitle{
+            display: inline;
+        }
+        .buttons{
+            display: inline;
+        
+        }
+        .buttons ul li{
+            float:left;
+            color:black;
+            border:6px solid black;
+            border-radius: 50%;
+            margin: 2px;
+            list-style-type: none;
+        }
+        .buttons a:link,a:visited,a:active{
+            color:black;
+        }
+        .span{
+            border:6px solid black;
+            border-radius: 50%;
+            margin: 2px;
+        }
+        #wrapper{
+            border: 2px solid black;
+            width:100%;
+            position:relative;
+            float:left;
+            overflow:hidden;
+            margin-right:10px;
+            
+            
+        }
+        #boxOne,#boxTwo,#boxThree{
+            position:relative;
+            width:100%;
+            margin:20px;
+            border:1px solid blue;
+        }
+        .cover2{
+            padding:0px;
+            margin:3px;
+            width:300px;
+            position:relative;
+            display:inline;
+        }
+        .cContent{
+            background-color:#0426d3;
+            position:relative;
+            width:40%;
+        }
+        #blog{
+            margin-bottom:100px;
+            position:relative;
+        }
+    </style>
+    <body>
+        <?php include("navbar.php"); ?>
+        <div id="about">
+            <div class="paratitle">about us</div>
+            <p>Civil Engineering Consortium is a Student Affair Committee of department of Civil Engineering, that is responsible to promote safe and intellectually stimulating learning environment that encourages not only the academic success of civil engineering students but also focus on personal development, sports and other extracurriculars student activities. With the chain of activities going round the year, students get ample opportunity to flash, flourosh and furnish their talents.</p>
+        
+        </div>
+        <div id="blog">
+            <div class="paratitle">blog<br></div>
+            <div id="wrapper">
+                <div id="boxOne" class="content">
+                    <div class="cover2"><img src="img/cover3.jpg" width="100%"></div>
+                    <div class="cContent"></div>
+                </div>
+                <div id="boxTwo" class="content">
+                </div>
+                <div id="boxThree" class="content">
+                </div>
+            </div>
+        
+        </div>
     
-		<?php endif ?>
-	</div>
-		
-</body>
+
+               
+            
+    
+    
+        <footer>
+            <div id="contact">
+                <p>Contact<br>Phone +91 9999 9999999<br>Email abc@gmaiil.com</p>
+            
+            
+          
+            </div>
+        
+        </footer>
+    
+    </body>
+
+
+
+
+
+
+
+
 </html>
---
+++ navbar.php
@@ -0,0 +1,17 @@
+<div class="container">
+            <div class="navbar">
+                <ul>
+                    <li><a href="INDEX.html" >home</a></li>
+                    <li><a href="#about">about</a></li>
+                    <li><a href="#blog">blog</a></li>
+                    <li><a href="#" name="team">team</a></li>
+                    <li><a href="#" name="contact">contact</a></li>
+                    <li><a href="#">more links</a></li>
+                </ul>
+            
+                
+            </div>
+    <div id="ceclogo">
+        <a href="index.html"><p>Civil Engineering<br>Consortium</p></a>
+    </div>
+</div>
