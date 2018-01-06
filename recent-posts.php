e28043a90211a794f130914fb6942c604f88539e
Varun Bhandia <varunbhandia@gmail.com>
2018-01-07 04:29:00 +0530

    Recents-Posts

---
+++ recent-posts.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EVENTS</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="css/navbar.css" type="text/css" rel="stylesheet">
+    </head>
+<style>
+a{
+  text-decoration: none;
+}
+.o{
+  margin: 5em 15em 0em;
+}
+.a{
+  margin: 0em 14em 0em 14em;
+}
+ .c{
+  background-color: #0A227B;
+  display: inline-block;
+  width: 100%;
+  height: 80%;
+  padding:0.5em;
+  
+ }
+ .d{
+  font-size: 1.2em;
+  }
+    .b{
+        margin-bottom: 1em;
+        height: 15em;
+    }
+ .b:hover{
+  box-shadow: 0 0.3em 0.5em;
+  cursor: pointer;
+ }
+ .e{
+  display: inline-block;
+   margin: 1em;
+ }
+ .e:hover{
+  box-shadow: 0 0.3em 0.5em;
+  cursor: pointer;
+ }
+ </style>
+<body>
+    <?php include("navbar.php"); ?>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-8">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>RECENT  POSTS</h3>
+            </div>
+            <div class="col-md-8">
+            </div>
+        </div>
+        <div class="row">
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+  
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+  
+</div>
+        <div class="row">
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+  
+    <div class="col-md-4">
+        <div class="b">
+            <a href="#">
+                <div class="c">
+                    <img src="#" style="width:100%">
+                </div>
+                <div class="d">
+                    <h5>WORKSHOP</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+  
+</div>
+    </div>
+    <div class="col-md-2">
+    </div>
+</body>
+</html>
