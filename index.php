109f3f3a5d9e1b97f74be71fd3e963100437e2ba
Varun Bhandia <varunbhandia@gmail.com>
2018-02-27 20:08:25 +0530

    more links

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
                       <li><a href="" style="padding-left: 3em;padding-right: 2em;">TEAM</a></li>
                       <li><a href="alumini-main-page.php" style="padding-left: 3em;padding-right: 2em;" >ALUMINI</a></li>
                       <li><a href="contact-us.php" style="padding-left: 3em;padding-right: 2em;" >CONTACT</a></li>
-                      <li class="dropdown">
-                      <a class="dropdown-toggle" data-toggle="dropdown" href="#" style="padding-left: 3em;padding-right: 9em;">MORE LINKS <span class="caret"></span></a>
+                      <li class="dropdown morelinks">
+                      <a class="dropdown-toggle " data-toggle="dropdown" href="#">MORE LINKS <span class="caret"></span></a>
                       <ul class="dropdown-menu">
                           <li><a href="#">Page 1-3</a></li>
                       </ul>
