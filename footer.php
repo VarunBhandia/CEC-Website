5a59232dc2260adcda427f52b9739f33ec4cecbd
Varun Bhandia <varunbhandia@gmail.com>
2018-08-16 19:31:35 +0530

    database

--- footer.php
+++ footer.php
@@ -1,46 +1,47 @@
 <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css"  href="css/footer.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <div class="social_media">
-                <a href="https://www.facebook.com/ceconsortium.iitr/" class="fa fa facebook"></a>
-            </div>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" type="text/css"  href="css/footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="social_media">
+    <a href="https://www.facebook.com/ceconsortium.iitr/" class="fa fa facebook"></a>
+</div>
 <footer class="footer-cec">
-        <div class="row">
-            <div class="col-xs-1 mobile"></div>
-            <div class="col-xs-9">
-            
+    <div class="row">
+        <div class="col-xs-1 mobile"></div>
+        <div class="col-xs-9">
+
             <div class="footer_content">
                 <h3>
                     CONTACT 
                 </h3>
                 <p class="number">
-                Phone: +91 9001126303
+                    Phone: +91 9001126303
                     <br>
-                        Email : varunbhandia@gmail.com</p>
-                    </div>
-                </div>
-            <div class="col-xs-2">
-                <div class="footer_content">
-                    <a href="" ><img src=""></a>
-                    <a href="" ><img src=""></a>
-                    </div>
+                    Email : varunbhandia@gmail.com</p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-1 mobile"></div>
-            <div class="col-md-9">
-                <div class="number2">
+        <div class="col-xs-2">
+            <div class="footer_content">
+                <a href="" ><img src=""></a>
+                <a href="" ><img src=""></a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-1 mobile"></div>
+        <div class="col-md-9">
+            <div class="number2">
                 Developed by 
                 <a href="https://github.com/VarunBhandia/" class="link_hover" style="color: #8488bb;"><b>Varun Bhandia</b></a>
-                    AND DESIGNED BY
-                    <a href="https://www.behance.net/AKASHSWF" class="link_hover" style="color: #8488bb;"><b>Akash Chandra</b></a> 
-                    <a href="#" class="fa fa facebook"></a>
-                </div>
+                AND DESIGNED BY
+                <a href="https://www.behance.net/AKASHSWF" class="link_hover" style="color: #8488bb;"><b>Akash Chandra</b></a> 
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+            </div>
             <div class="social_media">
                 <a href="https://www.facebook.com/ceconsortium.iitr/" class="fa facebook">Cec_Fb</a>
             </div>
-            </div>
-           
-</footer>
+        </div>
+
+        </footer>
